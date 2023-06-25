d59ba5f17057d305ff3eb6386b3ae42d4a4f3ea3
KleonDingeldein <121400442+KleonDingeldein@users.noreply.github.com>
2023-06-25 21:25:28 +0200

    Update Config.h
    struktur config geändert

--- Config.h
+++ Config.h
@@ -1,7 +1,3 @@
-//
-// Created by Justus Krenkel on 20.06.23.
-//
-
 #ifndef TUERSTEUERUNG_CONFIG_H
 #define TUERSTEUERUNG_CONFIG_H
 
@@ -14,27 +10,50 @@
 
 class Config {
 private:
-	// Einlesen der Config-Datei:
-	void read(std::string filename);
+	void objekt_anlegen(int pin, int port, std::string funktion, bool zustand, bool activationmode);
+	bool ein_Objekt_gesetzt(std::vector<Sensor> liste);
+	bool ein_Objekt_gesetzt(std::vector<Aktor> liste);
+	void alle_setzen(std::vector<Aktor> liste);
+	void alle_nullen(std::vector<Aktor> liste);
 
-	// Auswahl der Betriebsart
-	Sensor BW1, BW2;
-
-	// Vekotoren (keine Arrays) weil man angeblich über "name".emplace(Element) dynamische vergrößern kann
-	std::vector<Sensor> sensorliste;
-	std::vector<Aktor> aktorliste;
+	// Vekotoren der Hardware-Kategorien:
+        std::vector<Sensor> ELO, ELG, NTA, NTZ, NIT, NVT, BW1, BW2;
+	std::vector<Aktor> OPEN, CLOSE, LIGHT;
 
 protected:
 
 
 public:
-    Config(std::string datei);
+    Config(std::string filename);
     ~Config();
 
-    // Funktionen welche von Betriebsarten benötigtt werden:
-    
+    // Funktionen welche von Betriebsarten benötigt werden:
+    // Sensoraktivitaeten:
+    bool manuelles_oeffnen();
+    bool manuelles_schliessen();
+    bool person_vor_tuer();
+    bool person_in_tuer();
+    bool endlage_offen();
+    bool endlage_geschlossen();
+    bool mehrere_sensoren_aktiv();   //true sobald 2 sensoren in der liste mit status==1
 
+    // Rückgabe der Betriebswahl:
+    int betriebswahl();
+
+    // Aktoraktivitaeten:
+    bool oeffnet();          // true wenn ein oder mehrere motoren in der liste die tuer oeffnen
+    bool schliesst();        // true wenn ein oder mehrere motoren in der liste die tuer schliessen
+    bool lampe_leuchtet();   //nicht zwingend noetig
+
+    // Steuern der Aktoren:
+    void schliessen();         // alle oeffnermotoren aktiviern (in der liste status auf 1 setzen)
+    void oeffnen();            // alle schliessmotoren aktivieren (in der liste status auf 1 setzen)
+    void lampe_an();
+    void lampe_aus();
+    void motoren_stopp();
 };
 
 
 #endif //TUERSTEUERUNG_CONFIG_H
+
+
